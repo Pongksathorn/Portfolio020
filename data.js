d0e17bcdd630f8cc7e76d8050c70ef556624f82d
Auekun Bootsridouang <614259031@webmail.npru.ac.th>
2020-09-28 10:22:34 +0700

    Initial portfolio project Commit

---
+++ data.js
@@ -0,0 +1,15 @@
+const data={
+    "name" : "Auekun Bootsridouang",
+    "work" : "อายุ 20 ศึกษาชั้นประถมศึกษาที่ โรงเรียนบอสโกพิทักษ์ ศึกษาชั้นมัธยมศึกษาที่ โรงเรียนบอสโกพิทักษ์ ศึกษาชั้นอุดศึกษาที่ มหาลัยราชภัฏนครปฐม",
+    "text1" : "Hello ^_^",
+    "text2" : "I AM Wutz",
+    "text3" : "Student IN Major Softwere Engineering",
+    "aboutme" : {
+            "fullname" : "Auekun Bootsriddouang",
+            "Phone" : "064-8143564",
+            "Email" : "614259031@webmail.npru.ac.th",
+            "Address" : "85/1 หมู่ 1 ตำบล โพรงมะเดื่อ จังหวัด นครปฐม 73000"
+    }
+    
+};
+module.exports = data;
