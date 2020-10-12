9c3bd968ef6e5b4c8f24004f642d563dfaa8605c
Pongksathorn <614259020@webmail.npru.ac.th>
2020-10-12 22:28:11 +0700

    Pongksathorn

--- data.js
+++ data.js
@@ -1,14 +1,24 @@
 const data={
-    "name" : "Auekun Bootsridouang",
-    "work" : "อายุ 20 ศึกษาชั้นประถมศึกษาที่ โรงเรียนบอสโกพิทักษ์ ศึกษาชั้นมัธยมศึกษาที่ โรงเรียนบอสโกพิทักษ์ ศึกษาชั้นอุดศึกษาที่ มหาลัยราชภัฏนครปฐม",
+    "name" : "Pongksathorn Junha",
+    "education" : "ศึกษาชั้นประถมศึกษาที่ โรงเรียนบอสโกพิทักษ์ ศึกษาชั้นมัธยมศึกษาที่ โรงเรียนบอสโกพิทักษ์ ศึกษาชั้นอุดศึกษาที่ มหาลัยราชภัฏนครปฐม",
     "text1" : "Hello ^_^",
-    "text2" : "I AM Wutz",
+    "text2" : "I AM GolFz",
     "text3" : "Student IN Major Softwere Engineering",
     "aboutme" : {
-            "fullname" : "Auekun Bootsriddouang",
-            "Phone" : "064-8143564",
-            "Email" : "614259031@webmail.npru.ac.th",
-            "Address" : "85/1 หมู่ 1 ตำบล โพรงมะเดื่อ จังหวัด นครปฐม 73000"
+            "fullname" : "Pongksathorn Junha",
+            "age" : "20 year old",
+            "date" : "6 มีนาคม 2543",
+            "Phone" : "099-3486474",
+            "Email" : "614259020@webmail.npru.ac.th",
+            "Address" : "6 หมู่ 11 ตำบล โพรงมะเดื่อ จังหวัด นครปฐม 73000"
+      },
+      "work" : "ไม่เคยมีประสบการณ์ทำงานเลยครับ แย่จัง",
+    "skill" : {
+    "html" : "90%",
+    "css" : "80%",
+    "javascript" : "70%",
+    "photoshop" : "65%",
+    "adobepremierepro" : "60%",
     }
     
 };
--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-//const express = require('express');
-//const app = express();
- 
-//app.get('/',(req,res) => {//เเบบใหม่ name => {  }
-    //res.send("<h1>My Portfolio</h1>");//res.send ส่งข้อมูลไปหน้าเว็ป
-//});
-
-//app.listen(//Port3000 
-  //  3000,
-    //()=>{
-     //   console.log("listening to port 3000");
-    //}
-//);
 const express = require('express');
 const router =require('./routes/index');
 const app = express();
 const hbs = require('express-handlebars');
 const path = require('path');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.engine('hbs',hbs({extname:'hbs'}));
 app.set('view engine', 'hbs');
-app.use('/profile',router);
+app.use('/portfolio',router);
 app.use(express.static(path.join(__dirname,'public')));
 
 
